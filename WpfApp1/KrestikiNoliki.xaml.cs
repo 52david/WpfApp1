32c661da12f2cc5250be3c22226d26a3c3d735b2
khanchenkod25 <khanchenkod25@U00T1-01>
2026-04-29 12:58:23 +0200

    dodelano vybor s kem hoches igrat v krestikach

--- WpfApp1/KrestikiNoliki.xaml.cs
+++ WpfApp1/KrestikiNoliki.xaml.cs
@@ -18,19 +18,36 @@ namespace WpfApp1
     /// <summary>
     /// Interaction logic for KrestikiNoliki.xaml
     /// </summary>
-  
-    
+
+
     public partial class KrestikiNoliki : Window
     {
         Game game = new Game();
         Player player1;
         Player player2;
+
+        bool Bot = false;
+        Random r = new Random();
+        Game game1 = new Game();
+
+        Button[,] buttons = new Button[3, 3];
         public KrestikiNoliki()
         {
             InitializeComponent();
             player1 = player1;
             player2 = player2;
 
+            buttons[0, 0] = b00;
+            buttons[0, 1] = b01;
+            buttons[0, 2] = b02;
+
+            buttons[1, 0]=b10;
+            buttons[1, 1] = b11;
+            buttons[1, 2]=b12;
+
+            buttons[2,0]=b20;
+            buttons[2,1] = b21;
+            buttons[2,2] = b22;
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -39,9 +56,9 @@ namespace WpfApp1
             int rad = Grid.GetRow(btn);
             int kol = Grid.GetColumn(btn);
 
-            if (game.Chod(rad, kol)) 
+            if (game.Chod(rad, kol))
             {
-                btn.Content = game.TekushijPlayer; 
+                btn.Content = game.TekushijPlayer;
 
                 if (game.CheckWin())
                 {
@@ -50,10 +67,49 @@ namespace WpfApp1
                 }
 
                 game.SwitchPlayer();
+
+                if (Bot == true)
+                {
+                    ChodBota();
+
+                    if (game.CheckWin())
+                    {
+                        MessageBox.Show("Win " + game.TekushijPlayer);
+                        return;
+                    }
+                    game.SwitchPlayer();
+                }
+                
             }
         }
+        private void Friend_Click(object sender, RoutedEventArgs e)
+        {
+            Bot = false;
+            VyborPanel.Visibility = Visibility.Hidden;
+        }
+        private void Bot_click(object sender, RoutedEventArgs e)
+        {
+            Bot = true;
+            VyborPanel.Visibility = Visibility.Hidden;
+        }
 
-        
-        
+        private void ChodBota()
+        {
+            while (true)
+            {
+                int rad = r.Next(0, 3);
+                int kol = r.Next(0, 3);
+
+                if (game.Chod(rad, kol))
+                {
+                    buttons[rad, kol].Content = game.TekushijPlayer;
+                    
+                    break;                }
+                 }
+        }
+
+
+
+
     }
 }
